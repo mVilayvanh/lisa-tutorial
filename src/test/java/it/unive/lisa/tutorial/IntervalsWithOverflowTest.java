437209de29650e4251df223d26d8462b99dbd14d
yangl8 <yl1031318254@gmail.com>
2026-03-23 23:25:47 +0100

    Correction de la gestion des overflow et amélioration des tests

--- src/test/java/it/unive/lisa/tutorial/IntervalsWithOverflowTest.java
+++ src/test/java/it/unive/lisa/tutorial/IntervalsWithOverflowTest.java
@@ -1,7 +1,5 @@
 package it.unive.lisa.tutorial;
 
-import java.io.File;
-
 import org.junit.Test;
 
 import it.unive.lisa.AnalysisException;
@@ -18,17 +16,11 @@ public class IntervalsWithOverflowTest {
 
     @Test
     public void testIntervalsWithOverflowAnalysis() throws ParsingException, AnalysisException {
-        File input = new File("inputs/intervalsoverflow.imp");
-        System.out.println("INPUT EXISTS: " + input.exists());
-        System.out.println("INPUT ABS PATH: " + input.getAbsolutePath());
-
         Program program = IMPFrontend.processFile("inputs/intervalsoverflow.imp");
 
         LiSAConfiguration conf = new DefaultConfiguration();
         conf.workdir = "outputs/intervalsoverflow";
         conf.analysisGraphs = GraphType.HTML;
-
-        System.out.println("WORKDIR: " + new File(conf.workdir).getAbsolutePath());
 
         conf.abstractState = DefaultConfiguration.simpleState(
                 DefaultConfiguration.defaultHeapDomain(),
@@ -36,18 +28,6 @@ public class IntervalsWithOverflowTest {
                 DefaultConfiguration.defaultTypeDomain());
 
         LiSA lisa = new LiSA(conf);
-        System.out.println("BEFORE LISA RUN");
         lisa.run(program);
-        System.out.println("AFTER LISA RUN");
-
-        File out = new File(conf.workdir);
-        System.out.println("OUTPUT DIR EXISTS: " + out.exists());
-        if (out.exists()) {
-            File[] files = out.listFiles();
-            if (files != null) {
-                for (File f : files)
-                    System.out.println("OUTPUT ENTRY: " + f.getName());
-            }
-        }
     }
 }
